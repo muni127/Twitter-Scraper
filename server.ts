f02facb9eb461a5278f4584697d75342e40497c2
Duc Minh Ha <tranducminh_ha@yahoo.com>
2019-01-27 03:50:18 +1300

    improved query.

--- server.ts
+++ server.ts
@@ -5,7 +5,7 @@ console.log('\x1Bc'); // clear the console
 let bot = new ScrapperBot(); // create a twit bot
 
 bot.searchQuery = TwitterUtils.generateSearchQuery({
-    term: 'accordo.com',
+    //term: 'accordo.com',
     operators: [
         {
             operator: 'url:',
