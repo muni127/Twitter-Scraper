57701da22fbecadbd19b07a5204629db44b2cb95
Duc Minh Ha <tranducminh_ha@yahoo.com>
2019-01-27 03:33:39 +1300

    Increased polling rate to every 10 seconds.

--- src/configs.ts
+++ src/configs.ts
@@ -5,5 +5,5 @@
 export const appConfigs = {
     twitterSearchUrl: 'http://twitter.com/i/search/timeline',
     saveLocation: './result',
-    stream_interval_ms: 30000 // stream every 30 seconds
+    stream_interval_ms: 10000 // stream every 10 seconds
 };
